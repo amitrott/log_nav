42de62632a13c69326771ac447b3cca3d4848ac0
Angelo <lemu7>
2017-03-11 20:55:32 -0600

    relatively extensive commit
    - added output and dictionary (variable) support
    - go to end
    - debug logs and output separated
    - incorrect line shift on certain movements
    - tab/iteration
    - extract value from regex

--- Log_Nav.py
+++ Log_Nav.py
@@ -9,6 +9,8 @@ class Log_Nav():
         self.B = 0
         self.current_line = None
         self.filename = fname
+        self.output = []
+        self.dict = {}
         return
 
     def __enter__(self):
@@ -32,6 +34,13 @@ class Log_Nav():
         self.readline()
         return
 
+    def go_to_end(self):
+        self.readline()
+        while(self.current_line is not ""):
+            self.readline()
+        self.move_up()
+        return
+
     def readline(self):
         self.y += 1
         self.current_line = self.file.readline()
@@ -42,6 +51,8 @@ class Log_Nav():
         while(i < n):
             self.readline()
             i += 1
+            if self.current_line is "":
+                print("End of file reached, cannot move down!")
         return
 
     def move_up(self,n=1):
@@ -59,32 +70,96 @@ class Log_Nav():
 
     def move_down_until_regex(self,patt):
         regex = re.compile(patt)
-        self.move_down()
 
         while(regex.match(self.current_line) is None and self.current_line is not ""):
             self.move_down()
 
-        self.current_line = "End of file reached with no match for "+patt \
-                        if self.current_line is "" else self.current_line
+        if self.current_line is "":
+            print("End of file reached with no match for " + patt)
         return
 
     def move_up_until_regex(self,patt):
         regex = re.compile(patt)
-        self.move_up()
         i = self.y
 
         while (regex.match(self.current_line) is None and i >= 0):
             self.move_up()
             i -= 1
 
-        self.current_line = "Start of file reached with no match for " + patt \
-                        if i < 0 else self.current_line
+        if i < 0:
+            print("Start of file reached with no match for " + patt)
         return
 
+    def iter_tabbed(self):
+        regex = re.compile("^\s+(.*)")
 
-with Log_Nav("/home/angelo/PycharmProjects/log_nav/text_samples/line_counts.txt") as log_nav:
-    print(log_nav.current_line)
-    log_nav.move_down_until_regex("fifth")
-    print(log_nav.current_line)
+        self.move_down()
+        while(regex.search(self.current_line) is not None):
+            result = regex.match(self.current_line)
+            self.move_down()
+            yield result.group(1)
+
+        return
+
+    def exit_tabbed(self):
+        regex = re.compile("^[^\s]")
+
+        while(regex.search(self.current_line) is not None):
+            self.move_down()
+
+        return
+
+    def tabbed_regex(self,patt,name="regex_0"):
+        regex = re.compile(patt)
+        for line in self.iter_tabbed():
+            if(regex.search(line) is not None):
+                result = regex.match(line)
+                self.set_property(name,result.group(1))
+                self.exit_tabbed()
+                break
+        return
+
+    def append_to_output(self,text=None,whole_line=True):
+        if whole_line:
+            self.output.append(self.current_line)
+        else:
+            self.output.append(text)
+        return
+
+    def get_output(self):
+        return self.output
+
+    def get_dict(self):
+        return self.dict
+
+    def set_property(self,name,value):
+        self.dict[name] = value
+        return
+
+    def value_from_regex(self,patt,group=1):
+        regex = re.compile(patt)
+        result = regex.match(self.current_line)
+        return result.group(group)
+
+
+with Log_Nav("/home/angelo/PycharmProjects/log_nav/text_samples/line_counts_tabbed.txt") as log_nav:
+    log_nav.append_to_output()#print(log_nav.current_line)
+    log_nav.go_to_end()
+    log_nav.move_down()
+    log_nav.append_to_output()#print(log_nav.current_line)
     log_nav.move_up_until_regex(".*ir.*")
-    print(log_nav.current_line)
+    log_nav.append_to_output()#print(log_nav.current_line)
+    print(log_nav.get_output())
+
+    log_nav.set_property("test",log_nav.value_from_regex(".*(rd).*"))
+    print(log_nav.get_dict())
+
+    '''log_nav.go_to_start()
+    for match in log_nav.iter_tabbed():
+        print(match)'''
+
+    log_nav.go_to_start()
+    log_nav.tabbed_regex(".*dest=(.*)")
+
+    log_nav.append_to_output(log_nav.dict["regex_0"],False)
+    print(log_nav.get_output())
